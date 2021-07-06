fbc99e0dc4431c9e61e49ba433ae3d025de45c6b
Paulo Davi <paulo.araujo@cear.ufpb.br>
2021-07-05 22:57:46 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => console.log('Server is running in port 3333'));
